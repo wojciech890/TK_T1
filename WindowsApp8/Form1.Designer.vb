67600e50588d963ad3d0b49f98ef2e015410e096
wojciech890 <wojciech.olejarz@protonmail.com>
2023-11-12 14:49:03 +0100

    add genre listbox

--- WindowsApp8/Form1.Designer.vb
+++ WindowsApp8/Form1.Designer.vb
@@ -22,17 +22,41 @@ Partial Class LibraryFrom1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Genre = New System.Windows.Forms.Label()
+        Me.GenreListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
+        '
+        'Genre
+        '
+        Me.Genre.AutoSize = True
+        Me.Genre.Location = New System.Drawing.Point(57, 84)
+        Me.Genre.Name = "Genre"
+        Me.Genre.Size = New System.Drawing.Size(51, 13)
+        Me.Genre.TabIndex = 0
+        Me.Genre.Text = "Gatunek:"
+        '
+        'GenreListBox1
+        '
+        Me.GenreListBox1.FormattingEnabled = True
+        Me.GenreListBox1.Location = New System.Drawing.Point(60, 109)
+        Me.GenreListBox1.Name = "GenreListBox1"
+        Me.GenreListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.GenreListBox1.TabIndex = 1
         '
         'LibraryFrom1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 931)
+        Me.Controls.Add(Me.GenreListBox1)
+        Me.Controls.Add(Me.Genre)
         Me.Name = "LibraryFrom1"
         Me.Text = "Wypożyczalnia"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Genre As Label
+    Friend WithEvents GenreListBox1 As ListBox
 End Class
--- WindowsApp8/Form1.vb
+++ WindowsApp8/Form1.vb
@@ -1,5 +1,14 @@
 ﻿Public Class LibraryFrom1
+    Dim genres As New List(Of String) From
+        {"Fantastyka", "Biografie"}
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each genre As String In genres
+            GenreListBox1.Items.Add(genre)
+        Next
+    End Sub
+
+    Private Sub GenreListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles GenreListBox1.SelectedIndexChanged
 
     End Sub
 End Class

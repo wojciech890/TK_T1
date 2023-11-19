e27d7615bc70ffb9df0cc44ff45cf7eff99568cb
wojciech890 <wojciech.olejarz@protonmail.com>
2023-11-19 08:03:43 +0100

    init on empty xml

--- WindowsApp8/Form1.vb
+++ WindowsApp8/Form1.vb
@@ -5,7 +5,15 @@ Public Class LibraryFrom1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If System.IO.File.Exists("booksList.xml") = False Then
-            Console.Write("File Not Found, initialising empty book list")
+            Console.Write("File Not Found, initialising example book list")
+            books.AddRange({
+            New Book With {.Id = "683f28c5-62ac-4ed4-b72a-f883260b86d1", .Title = "Hobbit", .Genre = "Fantastyka", .Author = "J.R.R Tolkien", .Isbn = "832071608X", .State = "Dostępna"},
+            New Book With {.Id = "683f28c5-62ac-4ed4-b72a-f883260b86d2", .Title = "Hobbit", .Genre = "Fantastyka", .Author = "J.R.R Tolkien", .Isbn = "8373117113", .State = "Dostępna"},
+            New Book With {.Id = "217b1a67-de54-4606-8dfa-2e58d6107737", .Title = "Wiersze wszystkie", .Genre = "Poezja", .Author = "W. Szymborska", .Isbn = "9788324066391", .State = "Dostępna"},
+            New Book With {.Id = "8ecd031a-bce5-47a2-ab1a-1ae5db38ed30", .Title = "Fajdros", .Genre = "Filozofia", .Author = "Platon", .Isbn = "8388524569", .State = "Dostępna"},
+            New Book With {.Id = "09a0630f-dc89-47d5-86f5-c8c184e78220", .Title = "Silmarillion", .Genre = "Fantastyka", .Author = "J.R.R Tolkien", .Isbn = "8371698801", .State = "Dostępna"},
+            New Book With {.Id = "043bf7b4-2af2-4a1c-ae50-e7c93b873e78", .Title = "Silmarillion", .Genre = "Fantastyka", .Author = "J.R.R Tolkien", .Isbn = "8372458820", .State = "Dostępna"}
+        })
         Else
             books = ReadBookListFromXML()
         End If
